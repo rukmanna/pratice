f2ccec189731afd02064c0b69b4e6f508cf25ac5
Manali2508 <manali.shinde93@gmail.com>
2020-08-01 20:15:19 +0530

    python done

---
+++ python_conditional_statement.py
@@ -0,0 +1,18 @@
+# Conditional statements
+
+num = 5
+
+#case 1 --> greater than 10
+#case 2 --> less than 10
+
+
+if(num <= 10):
+    print("Pass")
+elif(num >= 10):
+    print("Greater")
+else:
+    print("Failed")
+    
+
+    
+    
---
+++ python_list.py
@@ -0,0 +1,6 @@
+# List
+weight = [10, 20,30,40]
+print("Old weight",weight)
+
+weight[1] = 50
+print("New weight",weight)
